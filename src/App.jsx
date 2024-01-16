69de1361541508b79a21d06ba7d8549f3387c2a6
Serikbolsyn Duisembay <ozat.online2020@gmail.com>
2024-01-16 05:01:12 +0500

    linters, first commmits

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
+import './App.css';
+import Button from './components/Button/Button';
+import JournalItem from './components/JournalItem/JournalItem';
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const data = [
+    {
+      title:  'Lorem ipsum dolor sit amet.',
+      text: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Alias, nemo.',
+      date: new Date()
+    },
+    {
+      title:  'Lorem ipsum dolor sit amet. vremeny data',
+      text: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Alias, nemo. hello world mello',
+      date: new Date()
+    }
+  ];
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h2>Загаловок</h2>
+      <p>Text</p>
+      <Button/>
+      <JournalItem
+        title = {data[0].title}
+        text={data[0].text}
+        date = {data[0].date}
+      />
+       <JournalItem
+        title = {data[1].title}
+        text={data[1].text}
+        date = {data[1].date}
+      />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
