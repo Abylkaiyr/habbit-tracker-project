936bd16ecf1cae952e90517c66ebdcdd50870948
Serikbolsyn <ozat.online2020@gmail.com>
2024-02-05 20:03:59 +0600

    lesson 41

--- src/App.jsx
+++ src/App.jsx
@@ -2,27 +2,30 @@
 import './App.css';
 import Body from './Layouts/Body/Body';
 import LeftPanel from './Layouts/LeftPanel/LeftPanel'; 
-import CardButton from './components/CardButton/CardButton';
+
 import Header from './components/Header/Header';
 import JournalAddButton from './components/JournalAddButton/JournalAddButton';
-import JournalItem from './components/JournalItem/JournalItem';
+
 import JournalList from './components/JournalList/JournalList';
 import JournalForm from './components/JournalForm/JournalForm';
 import { useState } from 'react';
 
 function App() {
 	const INITAL_DATA = [
+		// {
+		// 	id: 1,
+		// 	title:  'Lorem ipsum dolor sit amet.',
+		// 	text: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Alias, nemo.',
+		// 	date: new Date()
+		// },
+		// {
+		// 	id: 2,
+		// 	title:  'Lorem ipsum dolor sit amet. vremeny data',
+		// 	text: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Alias, nemo. hello world mello',
+		// 	date: new Date()
+		// },
 		{
-			title:  'Lorem ipsum dolor sit amet.',
-			text: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Alias, nemo.',
-			date: new Date()
-		},
-		{
-			title:  'Lorem ipsum dolor sit amet. vremeny data',
-			text: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Alias, nemo. hello world mello',
-			date: new Date()
-		},
-		{
+			id: 3,
 			title:  'Lorem ipsum dolor sit amet. vremeny data',
 			text: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Alias, nemo. hello world mello',
 			date: new Date()
@@ -33,27 +36,21 @@ function App() {
 		setItems(oldItems => [...oldItems, {
 			title: item.title,
 			text: item.text,
-			date: new Date(item.date)
+			date: new Date(item.date),
+			id:oldItems.length > 0 ?  Math.max(...oldItems.map(el=>el.id))+1 : 1
 		}]);
 	};
 
+	
 	const [items, setItems] = useState(INITAL_DATA); 
+
+	
 	return (
 		<div className='app'>
 			<LeftPanel>
 				<Header/>
 				<JournalAddButton/>
-				<JournalList>
-					{items.map(el=>(
-						<CardButton>
-							<JournalItem
-								title={el.title}
-								text={el.text}
-								date={el.date}						
-							/>
-						</CardButton>
-					))}					
-				</JournalList>
+				<JournalList items={ items }/>
 			</LeftPanel>
 			<Body>
 				<JournalForm onSubmit={addItem}  />
