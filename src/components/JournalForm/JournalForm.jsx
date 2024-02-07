5f2ee8d3fae6fe76aca9b2d84d8e914e8d4b03e5
Serikbolsyn <ozat.online2020@gmail.com>
2024-02-07 18:18:43 +0600

    updates

--- src/components/JournalForm/JournalForm.jsx
+++ src/components/JournalForm/JournalForm.jsx
@@ -1,6 +1,9 @@
 import styles from './JournalForm.module.css';
 import Button from '../Button/Button';
 import { useState } from 'react';
+import cn from 'classnames';
+
+
 function JournalForm({onSubmit}) {	
 	const [formValidState, setFormValidState] = useState(
 		{
@@ -48,10 +51,41 @@ function JournalForm({onSubmit}) {
 	return (	
 	
 		<form className={styles['journal-form']} onSubmit={addJournalItem}>
-			<input type='text' name='title' className={`${styles['input']} ${formValidState.title ? '' : styles['invalid']}`} />
-			<input type='date' name='date' className={`${styles['input']} ${formValidState.date ? '' : styles['invalid']}`} />   
-			<input type='text' name='tag'/>  
-			<textarea cols="30" rows="10" name='text' className={`${styles['input']} ${formValidState.text ? '' : styles['invalid']}`}></textarea> 
+			<div className=''>
+				<input type='text' name='title' className={
+					cn(styles['input-title'], {
+						[styles['invalid']]: !formValidState.title
+					})
+				} />
+			</div>
+
+			<div className={styles['form-row']}>
+				<label htmlFor='date' className={styles['form-label']}>
+					<img src="/calendar.svg" alt="Иконка каленьдаря" />
+					<span>Дата</span>
+				</label>
+				<input type='date' id='date' name='date' className={
+					cn(styles['input'], {
+						[styles['invalid']]: !formValidState.date
+					})
+				} />
+			</div>
+
+			
+			<div className={styles['form-row']}>
+				<label htmlFor='tag' className={styles['form-label']}>
+					<img src="/folder.svg" alt="Иконка папки" />
+					<span>Метки</span>
+				</label>
+				<input type='text' name='tag' id='tag' className={styles['input']} />
+			</div>
+
+			
+			<textarea cols="30" rows="10" name='text' className={
+				cn(styles['input'], {
+					[styles['invalid']]: !formValidState.text
+				})
+			}></textarea> 
 			<Button text='Сохранить' />
 		</form>	
 	);
